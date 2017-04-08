f3cbd55ae53554cde3f743eea29f39713fb11c53
Mr.Pain <ramil.talipov@gmail.com>
2017-04-08 22:06:24 +0300

    finally!

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,14 @@
 #encoding: UTF-8
-values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'] #Буби, Черви, Крести, Пики
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
-puts "#{values.sample} of #{suits.sample}"
-asdasd
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+suits = ['♥', '♦', '♣', '♠']
+
+puts "#{values.sample}#{suits.sample}"
